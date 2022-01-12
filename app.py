dc1862335be87dc8293ef46f7b6a5bc01085a95a
Rodrigo <rodrii.nicolas@outlook.com>
2022-01-12 00:20:25 -0300

    First conection to MongoDB

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
+from typing import Collection
 from flask import Flask
+import pymongo
 
 app = Flask(__name__)
 
+client = pymongo.MongoClient("mongodb+srv://root:lastoninas@clinic-management-datab.2df7e.mongodb.net/testDB?retryWrites=true&w=majority")
+db = client.testDB
+collection = db['test-collection']
+
 @app.route('/')
 def index():
-    return "Hello world!"
-
+    x = collection.insert_one({"hola":"recibido"}).inserted_id
+    return str(x)
+    
 if (__name__ == "__main__"):
     app.run(debug=True)
