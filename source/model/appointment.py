63619933683a8438e5707aa26401808c31118215
Nicolas De Maio <nicolas.demaio19@gmail.com>
2022-01-14 02:13:27 -0300

    refactors

--- source/model/appointment.py
+++ source/model/appointment.py
@@ -4,16 +4,10 @@ from source.model.utilities import *
 
 class Appointment:
 
-    def __init__(self, patient, doctor, datetime): # datetime = 14 01 2021 14:40:40
+    def __init__(self, patient, doctor, datetime):
         self.patient = patient
         self.doctor = doctor
         self.time_interval = self.__get_time_interval_from(datetime)
-
-    def starting_date(self):
-        return self.time_interval.from_date
-
-    def final_date(self):
-        return self.time_interval.to_date
 
     # Private methods
 
