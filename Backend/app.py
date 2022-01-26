f8918b985e45a2c0b6a557c1df517999ee195622
Nicolas De Maio <nicolas.demaio19@gmail.com>
2022-01-26 01:04:23 -0300

    Se realizaron dos peticiones HTTP a la API
    Co-authored-by: Rodrigo Iglesias <Rodrii53@users.noreply.github.com>

--- Backend/app.py
+++ Backend/app.py
@@ -3,9 +3,13 @@ from flask_restful import Api
 from configuration import perform_database_connection
 from source.main.controller.routes import initialize_routes
 from flask_jwt_extended import *
+from flask_cors import CORS
 
 app = Flask(__name__)
 api = Api(app)
+
+CORS(app, expose_headers='Authorization', allow_headers=["Content-Type", "Authorization", \
+    "Access-Control-Allow-Credentials"], supports_credentials=True)
 
 app.config['JWT_SECRET_KEY'] = 'jwt-secret-string'
 jwt = JWTManager(app)
@@ -14,4 +18,4 @@ initialize_routes(api)
 perform_database_connection()
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port="5000", debug=True)
--- frontend/src/views/LoginView.js
+++ frontend/src/views/LoginView.js
@@ -1,31 +1,41 @@
 import './LoginView.css'
 import React, {useState} from 'react'
-import { useLocalStorage } from '../services/useLocalStorage'
+import axios from 'axios';
 
 const LoginView = () => {
 
-  const [username, setUsername] = useLocalStorage('username', '')
+  const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
   const [user, setUser] = useState(null)
 
   const handleLogin = async (event) => {
     event.preventDefault()
-
-    // try {
-    //   const user = await loginService.login({username, password})
-    //   setUser(user)
-    //   setUsername('')
-    //   setPassword('')
-    // } catch (error) {
-    //   console.error(error)
-    // }
     
+    axios.post('http://localhost:5000/api/login', {
+      username,
+      password
+    })
+    .then(response => {
+      localStorage.setItem('token', response.headers['authorization'])
+    })
+    .catch(e => {
+        console.log(e)
+    })
   }
 
   const handleChange = (setValue) => {
     return (event) => setValue(event.target.value)
   }
 
+  const handleGetAppointments = () => {
+    axios.get('http://localhost:5000/api/appointments')
+    .then(response => {
+      console.log(response.data.data[0])
+    })
+    .catch(e => {
+        console.log(e)
+    })
+  }
 
   return (
     <>
@@ -38,6 +48,8 @@ const LoginView = () => {
         </label>
         <input type="submit" value="Ingresar"></input>
       </form>
+
+      <button onClick={handleGetAppointments}>Traer Turnos</button>
 
       <p>Username : {username}</p>
       <p>Password : {password}</p>
