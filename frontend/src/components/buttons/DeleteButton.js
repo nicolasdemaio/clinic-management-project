3666eb76769d830a293d62dcea41d29ef46ca9bb
Rodrigo <rodrii.nicolas@outlook.com>
2022-04-10 01:16:07 -0300

    Deleted decapred buttons

--- frontend/src/components/buttons/DeleteButton.js
+++
@@ -1,9 +0,0 @@
-import './DeleteButton.css'
-
-const DeleteButton = (props) => {
-    return (
-        <button className='delete-button'>Eliminar</button>
-    )
-}
-
-export default DeleteButton
--- frontend/src/components/buttons/ModifyButton.js
+++
@@ -1,9 +0,0 @@
-import './ModifyButton.css'
-
-const ModifyButton = (props) => {
-    return (
-        <button className='modify-button'>Modificar</button>
-    )
-}
-
-export default ModifyButton
