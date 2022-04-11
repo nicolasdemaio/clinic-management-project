27f116242144ad7ea47d32151b11e40af04b261c
Rodrigo <rodrii.nicolas@outlook.com>
2022-04-10 23:39:55 -0300

    First idea of Modals

--- frontend/src/components/modals/AddAppointment.js
+++ frontend/src/components/modals/AddAppointment.js
@@ -1,37 +1,61 @@
 import * as React from 'react';
-import { useState } from 'react';
+import Header from '../basics/Header';
 import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
 import Modal from '@mui/material/Modal';
+import { TextField } from '@mui/material';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import Select from '@mui/material/Select';
+import FormControl from '@mui/material/FormControl';
 
 const style = {
   position: 'absolute',
   top: '50%',
   left: '50%',
+  width: '80%',
+  height: '80%',
+  p: '30px',
+  overflow: 'hidden',
   transform: 'translate(-50%, -50%)',
-  width: 400,
-  bgcolor: 'background.paper',
-  border: '2px solid #000',
-  boxShadow: 24,
-  p: 4,
+  bgcolor: 'var(--primary-background)',
+  border: '1px solid var(--primary-color)',
+  borderRadius: '15px',
+  boxShadow: '10px 10px 60px rgb(0 0 0 / 40%)',
 };
 
 export const AddAppointment = (props) => {
   return (
-    <Modal
-      open={props.open}
-      onClose={props.onClose}
-      aria-labelledby="modal-modal-title"
-      aria-describedby="modal-modal-description"
-    >
-      <Box sx={style}>
-        <Typography id="modal-modal-title" variant="h6" component="h2">
-          Text in a modal
-        </Typography>
-        <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-          Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-        </Typography>
+    <Modal open={props.open} onClose={props.onClose}>
+      <Box className="add-appointment-container" sx={style}>
+        <Header
+          title="Turnos"
+          description="Citas médicas agendadas"
+          style={{ margin: '-30px -30px 0px -30px' }}
+        />
+        <br />
+        <div className="screen-content">
+          <h3>Registrar una cita</h3>
+          <FormControl margin="normal" style={{ width: '20%' }}>
+            <InputLabel id="demo-simple-select-label">
+              Tipo documento
+            </InputLabel>
+            <Select
+              labelId="demo-simple-select-label"
+              id="demo-simple-select"
+              label="Tipo documento"
+            >
+              <MenuItem value="DNI">D.N.I.</MenuItem>
+              <MenuItem value="LI">L.I.</MenuItem>
+              <MenuItem value="TH">Thirty</MenuItem>
+            </Select>
+          </FormControl>
+          <TextField
+            id="outlined-basic"
+            label="Identificación"
+            variant="outlined"
+            style={{ margin: '15px' }}
+          />
+        </div>
       </Box>
     </Modal>
   );
