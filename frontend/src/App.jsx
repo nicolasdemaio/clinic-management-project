a77b3d1851f9ce5c849a2a8099bdf9cdb678af86
Rodrigo <rodrii.nicolas@outlook.com>
2022-04-14 03:00:33 -0300

    Change create patient

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,7 +9,6 @@ import DoctorsScreen from './views/dashboard/doctors/DoctorsScreen';
 import PatientsScreen from './views/dashboard/patients/PatientsScreen';
 import DashboardScreen from './views/dashboard/home/DashboardScreen';
 import SinglePatientView from './views/dashboard/patients/SinglePatientView';
-import PatientCreationView from './views/dashboard/patients/PatientCreationView';
 
 function App() {
   return (
@@ -31,7 +30,6 @@ function App() {
               <Route path="home" element={<DashboardScreen />} />
               <Route path="appointments" element={<AppointmentsScreen />} />
               <Route path="patients" element={<PatientsScreen />} />
-              <Route path="patients/create" element={<PatientCreationView />} />
               <Route
                 path="patients/:patientId"
                 element={<SinglePatientView />}
---
+++ frontend/src/views/dashboard/patients/AddPatient.js
@@ -0,0 +1,81 @@
+import { Autocomplete, Stack, TextField } from '@mui/material';
+import SolidButton from '../../../components/buttons/SolidButton';
+import OutlinedButton from '../../../components/buttons/OutlinedButton';
+
+const styledBorder = {
+  '.css-9ddj71-MuiInputBase-root-MuiOutlinedInput-root.Mui-focused .MuiOutlinedInput-notchedOutline, .css-154xyx0-MuiInputBase-root-MuiOutlinedInput-root.Mui-focused .MuiOutlinedInput-notchedOutline':
+    {
+      borderColor: 'var(--primary-color)',
+    },
+  '.css-1sumxir-MuiFormLabel-root-MuiInputLabel-root.Mui-focused': {
+    color: 'var(--primary-color)',
+  },
+};
+
+export const AddPatient = (props) => {
+  return (
+    <>
+      <h3>Registrar un paciente</h3>
+      <div className="panel-grid">
+        <div className="left-pane">
+          <Stack component="form" noValidate sx={styledBorder}>
+            <TextField label="Nombre y apellido" variant="outlined" />
+            <TextField label="Domicilio" variant="outlined" />
+            <Autocomplete
+              disablePortal
+              id="combo-box-demo"
+              options={[
+                { label: 'D.N.I.' },
+                { label: 'LU' },
+                { label: 'NASHE' },
+              ]}
+              renderInput={(params) => <TextField {...params} label="Tipo" />}
+            />
+            <TextField
+              id="date"
+              label="Nacimiento"
+              type="date"
+              InputLabelProps={{
+                shrink: true,
+              }}
+            />
+            <br />
+            <TextField
+              id="datetime-local"
+              label="Appointment"
+              type="datetime-local"
+              sx={{ width: 250 }}
+              InputLabelProps={{
+                shrink: true,
+              }}
+            />
+          </Stack>
+        </div>
+        <div className="right-pane">
+          <Stack component="form" noValidate sx={styledBorder}>
+            <TextField
+              label="Correo electrónico"
+              type="email"
+              variant="outlined"
+            />
+            <TextField label="Número telefónico" variant="outlined" />
+            <TextField label="Número de documento" variant="outlined" />
+          </Stack>
+        </div>
+      </div>
+
+      <div style={{ display: 'flex' }}>
+        <SolidButton
+          onClick={(e) => {
+            alert('api register');
+          }}
+        >
+          Registrar
+        </SolidButton>
+        <OutlinedButton onClick={props.onClose}>Cancelar</OutlinedButton>
+      </div>
+    </>
+  );
+};
+
+export default AddPatient;
