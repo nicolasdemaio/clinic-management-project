064b41f622dfe5025326b44dd6723ca44441a804
riglesias1 <rodrii.nicolas@outlook.com>
2022-04-16 23:47:15 -0300

    Axios get with AuthToken and roles

--- frontend/src/api/patientsApi.js
+++ frontend/src/api/patientsApi.js
@@ -9,26 +9,15 @@ const config = {
   headers : {
     Authorization : authToken
   },
-  // params: {
-  //   roles: roles,
-  //   username: roles,
-  //   rol: roles
-  // },
-  // username: roles,
-  // roles: roles,
-  // rol: roles,
   data: {
-    // username: roles,
-    // rol: roles,
     roles: roles
   }
-}
+};
 
 const getPatientById = (aPatientId = '') => {
   return axios
     .get(PATIENTS_URL + "/" + aPatientId, config)
     .then((response) => {
-      console.log(response.status)
       return response.data;
     })
     .catch((e) => {
