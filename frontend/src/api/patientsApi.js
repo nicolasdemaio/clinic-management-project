9e9715d9d5e8f49850f75e98230db46e954f97a4
riglesias1 <rodrii.nicolas@outlook.com>
2022-04-16 23:39:29 -0300

    Ask Nicopro about data in request

--- frontend/src/api/patientsApi.js
+++ frontend/src/api/patientsApi.js
@@ -2,10 +2,33 @@ import axios from "./axios";
 
 const PATIENTS_URL = "api/patients";
 
+const authToken = localStorage.getItem('authToken');
+const roles = localStorage.getItem('roles');
+
+const config = {
+  headers : {
+    Authorization : authToken
+  },
+  // params: {
+  //   roles: roles,
+  //   username: roles,
+  //   rol: roles
+  // },
+  // username: roles,
+  // roles: roles,
+  // rol: roles,
+  data: {
+    // username: roles,
+    // rol: roles,
+    roles: roles
+  }
+}
+
 const getPatientById = (aPatientId = '') => {
   return axios
-    .get(PATIENTS_URL + "/" + aPatientId)
+    .get(PATIENTS_URL + "/" + aPatientId, config)
     .then((response) => {
+      console.log(response.status)
       return response.data;
     })
     .catch((e) => {
